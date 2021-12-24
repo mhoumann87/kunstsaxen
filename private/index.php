0a6cf1b78ed5d2f65a8f5544f0ba5ea199fe6486
Michael Houmann <mhoumann87@gmail.com>
2021-12-24 13:46:22 +0100

    Add redirect files, and mockup index for project

---
+++ private/index.php
@@ -0,0 +1,12 @@
+<?php
+ ini_set('display_errors', 1); 
+ ini_set('display_startup_errors', 1); 
+ error_reporting(E_ALL);
+ 
+require_once 'initialize.php';
+redirect_to('/kunstsaxen/public/index.php');
+echo 'this is PHP';
+?>
+
+<p>AThis is just normal HTML</p>
+
