54ad3ecbddf8291c17fab1eea9d5a99ee924e42f
Michael Houmann <mhoumann87@gmail.com>
2021-12-24 13:44:38 +0100

    Add initialiazing file

---
+++ private/initialize.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+* Turn on output buffering, to remove errors on loading pages, to do * processing of content before buffer is flushed
+* https://www.php.net/manual/en/function.ob-start.php
+*/
+ob_start();
+
+/*
+* Turn on error messaging for the project.
+  TODO: ADD IT TO THE php.ini file
+  ! THIS MUST BE DELETED BEFORE UPLOAD 
+*/
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
+/* 
+! REMEMBER TO DELETE  
+*/
+/*
+* assign file paths to PHP constants
+* __FILE__ returns the current path to this file
+* dirname() returns the path to the parent directory
+*/
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+define("PUBLIC_PATH", PROJECT_PATH.'/public');
+define("SHARED_PATH", PRIVATE_PATH.'/shared');
+
+// Add all the files needed for this project
+require_once 'functions.php';
