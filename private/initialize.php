fc03e4b290d8b4e091c087f9ffce4f29265d3cd3
Michael Houmann <mhoumann87@gmail.com>
2021-12-25 12:04:07 +0100

    Add functions to initialize.php

--- private/initialize.php
+++ private/initialize.php
@@ -11,9 +11,9 @@ ob_start();
   TODO: ADD IT TO THE php.ini file
   ! THIS MUST BE DELETED BEFORE UPLOAD 
 */
-ini_set('display_errors', 1); 
+/* ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 /* 
 ! REMEMBER TO DELETE  
 */
@@ -26,6 +26,16 @@ define("PRIVATE_PATH", dirname(__FILE__));
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", PROJECT_PATH.'/public');
 define("SHARED_PATH", PRIVATE_PATH.'/shared');
+
+/*
+* Assign the root URL to a PHP constant
+* - Do not need to include the domain
+* - Use same document root as webserver
+*/
+$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public');
+$public_end = $public_end + 7;
+$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+define('WWW_ROOT', $doc_root);
 
 // Add all the files needed for this project
 require_once 'functions.php';
